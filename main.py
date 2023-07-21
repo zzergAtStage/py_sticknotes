78685a53ec89406d3c95bff8743beab478f4cd7b
your_username <your_email@gmail.com>
2023-07-21 11:57:14 +0200

    Cleaning at it all

--- main.py
+++
@@ -1,9 +0,0 @@
-from notes_presenter import NotesPresenter
-
-def main():
-    file_path = "notes.json"
-    presenter = NotesPresenter(file_path)
-    presenter.run()
-
-if __name__ == "__main__":
-    main()
